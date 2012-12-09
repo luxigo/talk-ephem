14d240daed60bee702187298aba5fb4063c7f517
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2012-12-09 07:52:19 +0100

    install crontab, exclude README.md

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-tar c . --exclude .git --exclude install.sh --exclude install_dependencies.sh | tar xv --owner root --group root -C /
+#!/bin/sh
+tar c . --exclude .git --exclude README.md --exclude install.sh --exclude install_dependencies.sh | tar xv --owner root --group root -C /
+
+crontab -u root -l 2>/dev/null | sed -r -e /talk.ephem/d > /tmp/$$.tmp || exit
+echo "0 0 * * *  /etc/init.d/talk-ephem start" >> /tmp/$$.tmp
+crontab -u root /tmp/$$.tmp
+rm -f /tmp/$$.tmp 2>/dev/null
